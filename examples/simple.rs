d47e7cd2f80068e610109c2bec72d8180bebcb20
sokorototo <nyachiengatoto@gmail.com>
2025-10-24 13:08:20 +0300

    initial correct implementation

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,18 +1,108 @@
 use iced::widget;
 
-#[derive(Debug, Clone, Copy)]
-enum TestMessage {}
+#[derive(Debug, Clone)]
+enum Message {
+	FrameTick,
+	EditUrlInput(String),
+	CreateView,
+	ToggleWebview,
+	CreatedMainWindow(iced::window::Id),
+	UnsafeExtractedWindowHandle(usize),
+}
+
+struct State {
+	frames: usize,
+	url_input: String,
+
+	main_window: Option<iced::window::Id>,
+	webview_manager: iced_wry::IcedWebviewManager,
+	webview: Option<iced_wry::IcedWebview>,
+	webview_visible: bool,
+}
 
 fn main() {
+	fn new() -> (State, iced::Task<Message>) {
+		let open_window_task = iced::window::open(iced::window::Settings { ..Default::default() }).1;
+		(
+			State {
+				frames: 0,
+				url_input: Default::default(),
+
+				main_window: None,
+				webview_manager: iced_wry::IcedWebviewManager::init(),
+				webview: Default::default(),
+				webview_visible: true,
+			},
+			open_window_task.map(Message::CreatedMainWindow),
+		)
+	}
+
 	fn update(
-		state: &mut u64,
-		message: TestMessage,
-	) {
+		state: &mut State,
+		message: Message,
+	) -> iced::Task<Message> {
+		match message {
+			Message::FrameTick => {
+				state.frames += 1;
+			}
+			Message::EditUrlInput(chars) => {
+				state.url_input = chars;
+			}
+			Message::CreateView => {
+				if let Some(id) = state.main_window {
+					// create webview
+					return iced::window::run_with_handle(id, |handle| {
+						let raw = handle.as_raw();
+						Box::into_raw(Box::new(raw)) as usize
+					})
+					.map(Message::UnsafeExtractedWindowHandle);
+				}
+			}
+			Message::ToggleWebview => state.webview_visible = !state.webview_visible,
+			Message::UnsafeExtractedWindowHandle(ptr) => {
+				let raw = unsafe {
+					let ptr = ptr as *mut iced::window::raw_window_handle::RawWindowHandle;
+					Box::from_raw(ptr)
+				};
+				let window_handle = unsafe { iced::window::raw_window_handle::WindowHandle::borrow_raw(*raw) };
+
+				let mut attributes = iced_wry::wry::WebViewAttributes::default();
+				attributes.url = Some(state.url_input.clone());
+
+				let webview = state.webview_manager.new_webview(attributes, &window_handle).unwrap();
+				state.webview = Some(webview);
+			}
+			Message::CreatedMainWindow(id) => state.main_window = Some(id),
+		}
+
+		iced::Task::none()
 	}
 
-	fn view<'a>(value: &'a u64) -> widget::Column<'a, TestMessage> {
-		widget::column![widget::text("Simple display cuh"), widget::Space::with_height(25), widget::text("Add a little bit of spice")]
+	fn view<'a>(
+		state: &'a State,
+		_: iced::window::Id,
+	) -> widget::Column<'a, Message> {
+		widget::column![
+			widget::row![
+				widget::text!("Frames Rendered: {}", state.frames),
+				widget::Space::with_width(iced::Length::Fill),
+				widget::button("Toggle Visibility").on_press(Message::ToggleWebview)
+			],
+			widget::row![
+				widget::text_input("Enter a URL to open", state.url_input.as_str()).on_input(Message::EditUrlInput),
+				widget::button("Go To").on_press(Message::CreateView),
+				widget::Space::with_height(20)
+			]
+		]
+		.push_maybe(state.webview_visible.then(|| state.webview.as_ref().map(|w| w.view(iced::Length::Fill, iced::Length::Fill))).flatten())
 	}
 
-	iced::run::<u64, TestMessage, iced::Theme, iced::Renderer>("Simple Webview Test", update, view).unwrap();
+	fn subscription<'a>(_: &'a State) -> iced::Subscription<Message> {
+		iced::window::frames().map(|_| Message::FrameTick)
+	}
+
+	iced::daemon::<_, Message, iced::Theme, iced::Renderer>("Simple Webview Test", update, view)
+		.subscription(subscription)
+		.run_with(new)
+		.unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-use std::{collections, rc, sync, sync::mpsc};
+use std::{collections, sync};
 pub use wry;
 
-mod manager;
-mod message;
+pub enum IcedWryUpdate {
+	HideWebviews(Vec<usize>),
+}
 
 /// Stores state for synchronizing visibility and bounds for any managed [`webviews`](wry::WebView)
 #[derive(Debug, Clone)]
 pub struct IcedWebviewManager {
-	webviews: collections::BTreeMap<usize, rc::Weak<wry::WebView>>,
-	updater: mpsc::Sender<(usize, message::WebviewUpdateMessage)>,
+	webviews: collections::BTreeMap<usize, sync::Weak<wry::WebView>>,
+	// [previous, current] tracks two frame states, and only toggles off webview if state == [true, false]
+	display_tracker: sync::Arc<sync::Mutex<collections::BTreeMap<usize, [bool; 2]>>>,
 }
 
 impl IcedWebviewManager {
@@ -17,64 +19,106 @@ impl IcedWebviewManager {
 		WEBVIEW_COUNTER.fetch_add(1, sync::atomic::Ordering::Relaxed)
 	}
 
+	/// Initializes an iced [`subscription`](iced::Subscription) (for layout and visibility automation) and [`IcedWebviewManager`] (for Webview creation and sync).
+	/// The subscription must be installed into the runtime, to keep the webview's visibility updated
+	pub fn init() -> IcedWebviewManager {
+		IcedWebviewManager {
+			webviews: collections::BTreeMap::new(),
+			display_tracker: sync::Arc::new(sync::Mutex::new(collections::BTreeMap::new())),
+		}
+	}
+
 	/// Use [`get_oldest`](iced::window::get_oldest) to acquire the main window's [`Id`](iced::window::Id), then use [`run_with_handle`](iced::window::run_with_handle) to acquire the [`WindowHandle`](iced::window::raw_window_handle::WindowHandle)
 	pub fn new_webview(
 		&mut self,
 		mut attrs: wry::WebViewAttributes<'static>,
-		window_handle: iced::window::raw_window_handle::WindowHandle<'_>,
+		window_handle: &iced::window::raw_window_handle::WindowHandle<'_>,
 	) -> Result<IcedWebview, wry::Error> {
 		let id = IcedWebviewManager::increment_id();
 		attrs.visible = false;
 
 		// persist webview state
-		let webview = wry::WebView::new_as_child(&window_handle, attrs)?;
-		let inner = rc::Rc::new(webview);
+		let webview = wry::WebView::new_as_child(window_handle, attrs)?;
+		let webview = sync::Arc::new(webview);
 
-		self.webviews.insert(id, rc::Rc::downgrade(&inner));
+		self.webviews.insert(id, sync::Arc::downgrade(&webview));
+
+		// setup frame persistence state
+		if let Ok(mut guard) = self.display_tracker.lock() {
+			guard.entry(id).and_modify(|s| *s = [false, false]).or_insert([false, false]);
+		};
 
 		Ok(IcedWebview {
-			inner,
+			webview,
 			id,
-			updater: self.updater.clone(),
+			tracker: self.display_tracker.clone(),
 		})
+	}
+
+	/// Updates state for webviews tracked by [`display_tracker_task`](IcedWebviewManager::display_tracker_task)
+	pub fn update(
+		&mut self,
+		update: IcedWryUpdate,
+	) {
+		match update {
+			IcedWryUpdate::HideWebviews(ids) => {
+				for id in ids {
+					if let Some(weak) = self.webviews.get(&id) {
+						if let Some(webview) = weak.upgrade() {
+							if let Err(err) = webview.set_visible(false) {
+								eprintln!("Unable to update visibility for webview with id: {}\n{}", id, err)
+							};
+						}
+					} else {
+						eprintln!("Unable to find webview with id: {}", id)
+					}
+				}
+			}
+		}
+	}
+
+	/// Task that checks state for current webviews and syncs the state
+	pub fn webview_tracker_task(&self) -> iced::Task<IcedWryUpdate> {
+		unimplemented!()
 	}
 }
 
-#[derive(Clone)]
 pub struct IcedWebview {
-	inner: rc::Rc<wry::WebView>,
+	webview: sync::Arc<wry::WebView>,
+	tracker: sync::Arc<sync::Mutex<collections::BTreeMap<usize, [bool; 2]>>>,
 	id: usize,
-	updater: mpsc::Sender<(usize, message::WebviewUpdateMessage)>,
 }
 
 impl IcedWebview {
-	/// Acquire a [`IcedWebviewContainerElement`] for layout and rendering the [`webview`](wry::WebView) overlay
-	pub fn view(
-		&self,
+	/// Acquire a [`Element`](iced::Element) for layout and rendering the [`webview`](wry::WebView) overlay
+	pub fn view<'a, Message, Theme>(
+		&'a self,
 		width: impl Into<iced::Length>,
 		height: impl Into<iced::Length>,
-	) -> IcedWebviewContainerElement {
-		IcedWebviewContainerElement {
-			inner: self.clone(),
+	) -> iced::Element<'a, Message, Theme> {
+		let inner = IcedWebviewContainerElement {
+			inner: self,
 			width: width.into(),
 			height: height.into(),
-		}
+		};
+
+		iced::Element::new(inner)
 	}
 }
 
 impl AsRef<wry::WebView> for IcedWebview {
 	fn as_ref(&self) -> &wry::WebView {
-		&self.inner
+		&self.webview
 	}
 }
 
-pub struct IcedWebviewContainerElement {
-	inner: IcedWebview,
+pub(crate) struct IcedWebviewContainerElement<'a> {
+	inner: &'a IcedWebview,
 	width: iced::Length,
 	height: iced::Length,
 }
 
-impl<Message, Theme, R: iced::advanced::Renderer> iced::advanced::Widget<Message, Theme, R> for IcedWebviewContainerElement {
+impl<'a, Message, Theme, R: iced::advanced::Renderer> iced::advanced::Widget<Message, Theme, R> for IcedWebviewContainerElement<'a> {
 	fn size(&self) -> iced::Size<iced::Length> {
 		iced::Size {
 			width: self.width,
@@ -88,19 +132,7 @@ impl<Message, Theme, R: iced::advanced::Renderer> iced::advanced::Widget<Message
 		_renderer: &R,
 		limits: &iced::advanced::layout::Limits,
 	) -> iced::advanced::layout::Node {
-		let node = iced::advanced::layout::Node::new(limits.resolve(self.width, self.height, iced::Size::ZERO));
-		let bounds = node.bounds();
-
-		let rect = wry::Rect {
-			position: wry::dpi::Position::Logical(wry::dpi::LogicalPosition::new(bounds.x.into(), bounds.x.into())),
-			size: wry::dpi::LogicalSize::<f64>::new(bounds.width.into(), bounds.height.into()).into(),
-		};
-
-		if let Err(err) = self.inner.as_ref().set_bounds(rect) {
-			eprintln!("Unable to set bounds for webview with id: {}\n{}", self.inner.id, err)
-		};
-
-		node
+		iced::advanced::layout::atomic(limits, self.width, self.height)
 	}
 
 	fn draw(
@@ -109,17 +141,35 @@ impl<Message, Theme, R: iced::advanced::Renderer> iced::advanced::Widget<Message
 		_renderer: &mut R,
 		_theme: &Theme,
 		_style: &iced::advanced::renderer::Style,
-		_layout: iced::advanced::Layout<'_>,
+		layout: iced::advanced::Layout<'_>,
 		_cursor: iced::advanced::mouse::Cursor,
 		_viewport: &iced::Rectangle,
 	) {
-		// webview is rendered as an overlay, the overlay is
-		self.inner.updater.send((self.inner.id, message::WebviewUpdateMessage::PersistFrame)).unwrap()
-	}
-}
+		let bounds = layout.bounds();
 
-impl Drop for IcedWebviewContainerElement {
-	fn drop(&mut self) {
-		self.inner.updater.send((self.inner.id, message::WebviewUpdateMessage::DecayFrame)).unwrap()
+		let rect = wry::Rect {
+			position: wry::dpi::Position::Logical(wry::dpi::LogicalPosition::new(bounds.x.into(), bounds.y.into())),
+			size: wry::dpi::LogicalSize::<f64>::new(bounds.width.into(), bounds.height.into()).into(),
+		};
+
+		if let Err(err) = self.inner.as_ref().set_bounds(rect) {
+			eprintln!("Unable to set bounds for webview with id: {}\n{}", self.inner.id, err)
+		};
+
+		if let Ok(mut guard) = self.inner.tracker.lock() {
+			guard
+				.entry(self.inner.id)
+				.and_modify(|s| {
+					s.swap(0, 1);
+					s[1] = true;
+				})
+				.or_insert([false, true]);
+		} else {
+			eprintln!("Unable to acquire lock for internal Arc<Mutex> tracker")
+		};
+
+		if let Err(err) = self.inner.as_ref().set_visible(true) {
+			eprintln!("Unable to update visibility for webview with id: {}\n{}", self.inner.id, err)
+		};
 	}
 }
